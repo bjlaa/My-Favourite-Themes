60a9881d891b0ec370043ac057ad1d1d43c64038
Benjamin <benjamin.j.arias@gmail.com>
2016-04-22 18:49:07 +0200

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-var glawie = 2;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Audiolet from '../audiolet';
+
+class App extends React.Component {
+	AudioletApp() {
+        this.audiolet = new Audiolet();
+        this.sine = new Sine(this.audiolet, 440);
+        this.sine.connect(this.audiolet.output);
+	}
+	render() {
+		return(
+			<div className="piano" onClick={this.AudioletApp}>
+
+			</div>
+		)
+	}
+}
+
+/*
+	Renders to the DOM element with the id "main"
+*/
+
+var main= document.querySelector("#main");
+ReactDOM.render(<App />, main);
